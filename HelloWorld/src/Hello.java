a400c338349e73376d5eca34816dbc7667879b3c
User <User@DESKTOP-VNGL1B2>
2021-07-22 16:11:52 +0900

    7/22

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		if(str.equals("JDS")) {
+			System.out.println("Jeon Dong Su");
+		}
+	}
+
+}
